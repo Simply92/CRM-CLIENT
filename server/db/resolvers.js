e40c74501ca34ca35a274d15d74ad369acf414ec
Simply92 <pedrouner92@gmail.com>
2024-06-11 19:53:56 -0300

    jwt implementacion para usuarios

--- server/db/resolvers.js
+++ server/db/resolvers.js
@@ -1,9 +1,20 @@
 const Usuario = require('../models/Usuario')
 const bcryptjs = require('bcryptjs')
+require('dotenv').config({ path: '.env'})
+const jwt = require('jsonwebtoken')
+
+const crearToken = (usuario, secreta, expiresIn) => {
+    const {id, email, nombre, apellido } = usuario
+    return jwt.sign({id, email, nombre, apellido}, secreta, {expiresIn})
+}
+
 
 const resolvers = {
     Query:{
-        obtener: () => "algo"
+        obtenerUsuario: async(_, {token}) => {
+            const usuarioId = await jwt.verify(token, process.env.SECRETA)
+            return usuarioId
+        }
     },
     Mutation:{
         nuevoUsuario: async (_, { input }) => {
@@ -24,6 +35,25 @@ const resolvers = {
                 console.log(error)
             }
         },
+        autenticarUsuario: async(_, {input}) => {
+            const {email, password} = input
+
+            const existeUsuario = await Usuario.findOne({email})
+
+            if(!existeUsuario){
+                throw new Error('El usuario no existe')
+            }
+
+            const passwordCorrecto = await bcryptjs.compare( password, existeUsuario.password )
+            
+            if(!passwordCorrecto){
+                throw new Error('El password es incorrecto')
+            }
+
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, "24h")
+            }
+        }
     }
 }
 
--- server/db/schema.js
+++ server/db/schema.js
@@ -9,6 +9,10 @@ const typeDefs = gql`
         email: String
         creado: String
     }
+
+    type Token {
+        token: String
+    }
     
     input UsuarioInput {
         nombre: String!
@@ -17,12 +21,18 @@ const typeDefs = gql`
         password: String!
     }
 
+    input AutenticarInput{
+        email: String!
+        password: String!
+    }
+
     type Query{
-        obtener: String
+        obtenerUsuario(token: String!) : Usuario
     }
 
     type Mutation {
         nuevoUsuario(input: UsuarioInput) : Usuario
+        autenticarUsuario(input: AutenticarInput): Token
     }
 `
 
